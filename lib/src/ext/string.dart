e2dd499d4c77a5f225bdd762c5a498620cfa7988
eoe2005 <eoe2005@hotmail.com>
2025-03-12 15:37:44 +0800

    继续修改代码

--- lib/src/ext/string.dart
+++ lib/src/ext/string.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
 
+import '../utils/location.dart';
 
 extension StringExt on String {
+  
   //显示多语言功能
-  String location(){
-    return "";
+  String location(BuildContext context){
+    return GLocalizations.of(context).get(this);
   }
 }
--- lib/src/widget/dev.dart
+++ lib/src/widget/dev.dart
@@ -17,69 +17,69 @@ class _DevState extends State<Dev> {
     );
   }
 }
-//显示请求日志
-class DevLog extends StatefulWidget {
-  const DevLog({ Key? key }) : super(key: key);
+// //显示请求日志
+// class DevLog extends StatefulWidget {
+//   const DevLog({ Key? key }) : super(key: key);
 
-  @override
-  _DevLogState createState() => _DevLogState();
-}
+//   @override
+//   _DevLogState createState() => _DevLogState();
+// }
 
-class _DevLogState extends State<DevLog> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-     appBar: AppBar(
-       title: Text('请求日志'),
-       actions: [
-         IconButton(onPressed: () {
-           ApiLogs.clear();
-           setState(() {});
-         }, icon: Icon(Icons.delete))
-       ],
-     ),
-     body: ListView.builder(
-       itemCount: ApiLogs.length,
-       itemBuilder: (context, index) {
-         return ListTile(
-           title: Text(ApiLogs.logs[index].url),
-           subtitle: Text(ApiLogs.logs[index].time),
-           onTap: () {
-             Navigator.push(context, MaterialPageRoute(builder: (context) => DevLogInfo(info: ApiLogs.logs[index])));
-           }
-         );
-       }
-     ) 
-    );
-  }
-}
-//显示请求日志详情
-class DevLogInfo extends StatefulWidget {
-  final ApiLogInfo info;
-  const DevLogInfo({ Key? key ,required ApiLogInfo this.info}) :super(key: key);
+// class _DevLogState extends State<DevLog> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//      appBar: AppBar(
+//        title: Text('请求日志'),
+//        actions: [
+//          IconButton(onPressed: () {
+//            ApiLogs.clear();
+//            setState(() {});
+//          }, icon: Icon(Icons.delete))
+//        ],
+//      ),
+//      body: ListView.builder(
+//        itemCount: ApiLogs.length,
+//        itemBuilder: (context, index) {
+//          return ListTile(
+//            title: Text(ApiLogs.logs[index].url),
+//            subtitle: Text(ApiLogs.logs[index].time),
+//            onTap: () {
+//              Navigator.push(context, MaterialPageRoute(builder: (context) => DevLogInfo(info: ApiLogs.logs[index])));
+//            }
+//          );
+//        }
+//      ) 
+//     );
+//   }
+// }
+// //显示请求日志详情
+// class DevLogInfo extends StatefulWidget {
+//   final ApiLogInfo info;
+//   const DevLogInfo({ Key? key ,required ApiLogInfo this.info}) :super(key: key);
 
-  @override
-  _DevLogInfo createState() => _DevLogInfo();
-}
+//   @override
+//   _DevLogInfo createState() => _DevLogInfo();
+// }
 
-class _DevLogInfo extends State<DevLogInfo> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('请求日志'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text('请求地址：${widget.info.url}'),
-            Text('请求方法：${widget.info.method}'),
-            Text('请求参数：${widget.info.params}'),
-            Text('请求时间：${widget.info.time}'),
-            Text('响应数据：${widget.info.response}'),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class _DevLogInfo extends State<DevLogInfo> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('请求日志'),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             Text('请求地址：${widget.info.url}'),
+//             Text('请求方法：${widget.info.method}'),
+//             Text('请求参数：${widget.info.params}'),
+//             Text('请求时间：${widget.info.time}'),
+//             Text('响应数据：${widget.info.response}'),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
